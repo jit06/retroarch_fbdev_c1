372e8fdc81beb0f8b53d27664b6dd12afa13b0ae
jit06 <julien@bluemind.org>
2020-03-05 21:43:27 +0000

    moved disabled services

--- system_optimize.sh
+++ system_optimize.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 function system_optimize() {
-    
+
+    # disable some unwanted services
+    systemctl disable rsyslog
+    systemctl disable ModemManager
+    systemctl disable pppd-dns.service
+    systemctl disable wpa_supplicant 
+    systemctl disable loadcpufreq.service    
+
     # change boot.ini (change res, overclock, quiet boot, etc.)
-    patch -R /media/boot/boot.ini $ODROIDC1_BUILD_PATH/patches/boot.ini.patch
+    patch /media/boot/boot.ini $ODROIDC1_BUILD_PATH/patches/boot.ini.patch
 
     # journald in memory (instead of /var/log 
     sed -i "s/#Storage=auto/Storage=volatile/" /etc/systemd/journald.conf
