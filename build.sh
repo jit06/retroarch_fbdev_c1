c31457eeca83aca05ccdaa8be808fb4eab7de6b5
jit06 <julien@bluemind.org>
2020-03-05 21:40:28 +0000

    added system splash and ending message

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ ODROIDC1_BUILD_PATH=/root/retroarch_fbdev_c1/
 source "$ODROIDC1_BUILD_PATH/retroarch_packages.sh"
 source "$ODROIDC1_BUILD_PATH/system_prepare.sh"
 source "$ODROIDC1_BUILD_PATH/system_optimize.sh"
+source "$ODROIDC1_BUILD_PATH/system_splash.sh"
 source "$ODROIDC1_BUILD_PATH/retroarch_install.sh"
 source "$ODROIDC1_BUILD_PATH/retroarch_optimize.sh"
 
@@ -15,6 +16,8 @@ source "$ODROIDC1_BUILD_PATH/retroarch_optimize.sh"
 system_prepare()
 # tweak for fast boot and fewer disk write
 system_optimize()
+# create uInitrd with splash image
+system_splash()
 # compile retroarch and emulator cores
 retroarch_install()
 # change some settings to make emulators faster on the C1/C0
@@ -22,3 +25,8 @@ retroarch_optimize()
 
 # clean up
 sudo apt-get clean
+
+echo ""
+echo "============================================"
+echo "if everything went ok, you should reboot now"
+echo "============================================"
