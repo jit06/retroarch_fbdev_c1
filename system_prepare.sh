0e369e6667985c5d24a9cbaf449e818b17fd19fc
jit06 <julien@bluemind.org>
2020-03-05 21:41:57 +0000

    added uboot logo management

--- system_prepare.sh
+++ system_prepare.sh
@@ -4,24 +4,24 @@ function system_prepare() {
 
     # update / upgrade the base system
     apt-get update && apt-get upgrade apt-get dist-upgrade 
-
-    
+  
     # install needed packages
-    apt-get install git mali-fbdev plymouth
-    
-
-    # disable some unwanted services
-    systemctl disable rsyslog
-    systemctl disable ModemManager
-    systemctl disable pppd-dns.service
-    systemctl disable wpa_supplicant 
-    systemctl disable loadcpufreq.service
-
-    
-    # allow overscan management and boot logo display
+    apt-get install git mali-fbdev u-boot-tools busybox fbi
+            
+    # boot logo display
     cp $ODROIDC1_BUILD_PATH/splash.png /media/boot/
     cp $ODROIDC1_BUILD_PATH/boot-logo.bmp /media/boot/
+    echo "/dev/mmcblk0 0x80000 0x8000" > /etc/fw_env.config
+    fw_setenv preloadlogo 'video open;video clear; video dev open ${outputmode};fatload mmc 0:1 ${loadaddr_logo} boot-logo.bmp;bmp display ${loadaddr_logo}; bmp scale'
 
+    # default to 720x480 in uboot
+    fw_setenv hdmimode 480p60hz
+    fw_setenv outputmode 480p
+    fw_setenv fb_width=720
+    fw_setenv fb_height=480
+
+
+    # allow overscan management throught overscan file in /media/boot
     sed -i '/exit 0/d' /etc/rc.local
     echo '
     if [ -f /media/boot/overscan ]; then
