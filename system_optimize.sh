0bc6149cfa4c2e869f87ba55f76864f49e62aa1f
jit06 <julien@bluemind.org>
2020-03-08 13:02:03 +0000

    added status messages

--- system_optimize.sh
+++ system_optimize.sh
@@ -3,6 +3,10 @@
 function system_optimize() {
 
     # disable some unwanted services
+    echo ""
+    echo "============================================"
+    echo " disable unwanted services...               "
+    echo "============================================"
     systemctl disable rsyslog
     systemctl disable ModemManager
     systemctl disable pppd-dns.service
@@ -10,18 +14,30 @@ function system_optimize() {
     systemctl disable loadcpufreq.service    
 
     # change boot.ini (change res, overclock, quiet boot, etc.)
+    echo ""
+    echo "============================================"
+    echo " patch boot.ini...                          "
+    echo "============================================"
     patch /media/boot/boot.ini $ODROIDC1_BUILD_PATH/patches/boot.ini.patch
 
+    echo ""
+    echo "============================================"
+    echo " logs to memory and serial...               "
+    echo "============================================"
     # journald in memory (instead of /var/log 
     sed -i "s/#Storage=auto/Storage=volatile/" /etc/systemd/journald.conf
 
     # journald output to serial instead of console 
     sed -i "s/#TTYPath=\/dev\/console/TTYPath=\/dev\/ttyS0/" /etc/systemd/journald.conf
-
-    # /var/logs as tmpfs
+ 
+   # /var/logs as tmpfs
     rm -Rf /var/log/*
     echo "tmpfs /var/log tmpfs nodev,nosuid,noatime,size=20M 0 0" >> /etc/fstab
 
+    echo ""
+    echo "============================================"
+    echo " change mount options for root fs...        "
+    echo "============================================"
     # optimize mount option for flash usage
     sed -i "s/ext4 defaults/ext4 noatime,discard,defaults/" /etc/fstab
 
