bb15bfb99c70e96a66a1239e0460bd518c9328b7
jit06 <julien@bluemind.org>
2020-03-08 19:41:24 +0000

    array was badly defined

--- retroarch_packages.sh
+++ retroarch_packages.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # retropie packages to compile and install
-PACKAGES=("retroarch"
-	 "amiberry"
-         "lr-atari800"
-         "lr-beetle-ngp"
-         "lr-beetle-pce-fast"
-         "lr-beetle-supergrafx"
-         "lr-beetle-wswan"
-         "lr-caprice32"
-         "lr-fbneo"
-         "lr-fceumm"
-         "lr-flycast"
-         "lr-fuse"
-         "lr-gambatte"
-         "lr-genesis-plus-gx"
-         "lr-handy"
-         "lr-mame2003"
-         "lr-mgba"
-         "lr-mrboom"
-         "lr-mupen64plus"
-         "lr-nestopia"
-         "lr-pcsx-rearmed"
-         "lr-picodrive"
-         "lr-prboom"
-         "lr-prosystem"
-         "lr-snes9x2005"
-         "lr-snes9x2010"
-         "lr-stella2014"
-         "lr-tyrquake"
-         "lr-vecx"
-         "lr-xrick")
+PACKAGES=(retroarch
+	 amiberry \
+         lr-atari800 \
+         lr-beetle-ngp \
+         lr-beetle-pce-fast \
+         lr-beetle-supergrafx \
+         lr-beetle-wswan \
+         lr-caprice32 \
+         lr-fbneo \
+         lr-fceumm \
+         lr-flycast \
+         lr-fuse \
+         lr-gambatte \
+         lr-genesis-plus-gx \
+         lr-handy \
+         lr-mame2003 \
+         lr-mgba \
+         lr-mrboom \
+         lr-mupen64plus \
+         lr-nestopia \
+         lr-pcsx-rearmed \
+         lr-picodrive \
+         lr-prboom \
+         lr-prosystem \
+         lr-snes9x2005 \
+         lr-snes9x2010 \
+         lr-stella2014 \
+         lr-tyrquake \
+         lr-vecx \
+         lr-xrick)
