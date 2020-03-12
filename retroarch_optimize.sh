aba7c46c4376b327a9ca856a0777ff2b0de9d5e9
jit06 <julien@bluemind.org>
2020-03-12 23:04:44 +0000

    added custom hostpref.conf

--- retroarch_optimize.sh
+++ retroarch_optimize.sh
@@ -2,8 +2,10 @@
 
 function retroarch_optimize() {
 
-    # amiberry : set FAST_COPPER=TRUE in all <hardware> to make a500 games playable on C1
+    # amiberry : set FAST_COPPER=TRUE in all <hardware> to make a500 games playables on C1
     sed -i "s/<hardware>/<hardware>FAST_COPPER=TRUE/" /opt/retropie/emulators/amiberry/whdboot/game-data/whdload_db.xml
+    
+    cp $ODROIDC1_BUILD_PATH/configs/hostprefs.conf /opt/retropie/emulators/amiberry/whdboot/
 
     # copy retroarch config to make them run fine on C1
     cp $ODROIDC1_BUILD_PATH/configs/retroarch* /root/.config/retroarch/
