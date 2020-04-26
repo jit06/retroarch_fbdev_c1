e3d41861c2ecb43b505245a637621c80e92a1ee3
jit06 <julien@bluemind.org>
2020-04-26 20:28:52 +0000

    delete build logs and playlists

--- utils/prepare4img.sh
+++ utils/prepare4img.sh
@@ -13,6 +13,8 @@ rm -Rf /root/.local
 rm /resize.log
 rm /boot/*
 rm /root/.bash_history
+rm /root/retroarch_fbdev_c1/buildlogs/*
+rm /root/.config/retroarch/playlists/*
 
 cp aafirstboot .first_boot /
 
