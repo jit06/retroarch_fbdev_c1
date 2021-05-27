8dd757c467727a04d7e10eb740f1c224fa666aa2
root <julien@bluemind.org>
2021-05-27 07:56:49 +0000

    now install automatically the needed kernel

--- enable_batterymon.sh
+++ enable_batterymon.sh
@@ -14,11 +14,25 @@ if [ ! -d /root/.config/retroarch/assets/xmb/retroactive/png ]; then
     exit 0
 fi
 
+
+echo ""
+echo "============================================"
+echo " install kernel with battery support        "
+echo "============================================"
+cd /root 
+wget https://github.com/jit06/linux/releases/download/V2.1/linux-odroic-noconsole-battery.tar.gz
+tar -zxf linux-odroic-noconsole-battery.tar.gz
+cd linux-odroic-noconsole-battery
+cp uImage /media/boot
+cp -R lib/* /lib/
+
+
 echo ""
 echo "============================================"
 echo " install custom battery icons               "
 echo "============================================"
 cp $ODROIDC1_BUILD_PATH/skin/Retroarch/Battery_icons/* /root/.config/retroarch/assets/xmb/retrosystem/png/
+
 
 echo ""
 echo "============================================"
@@ -28,3 +42,5 @@ cp $ODROIDC1_BUILD_PATH/scripts/odroid_battery.sh /opt/
 cp $ODROIDC1_BUILD_PATH/systemd/odroid-battery.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable odroid-battery
+
+cd $ODROIDC1_BUILD_PATH
