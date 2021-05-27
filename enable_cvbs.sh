ea620433340416746b510bc0d931ea8b3953df03
root <julien@bluemind.org>
2021-05-27 07:18:03 +0000

    added cvbs output option

---
+++ enable_cvbs.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# build path for this build script
+ODROIDC1_BUILD_PATH=/root/retroarch_fbdev_c1
+
+echo ""
+echo "============================================"
+echo " change boot.ini                            "
+echo "============================================"
+sed -i -e 's/setenv m "480p"/setenv m "480cvbs" /g' /media/boot/boot.ini
+
+
+echo ""
+echo "============================================"
+echo " change uboot                               "
+echo "============================================"
+fw_setenv outputmode 480cvbs
+
