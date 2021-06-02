8b29349ef62ac34253e01c36521181a69f4b8c6c
root <julien@bluemind.org>
2021-06-02 20:56:44 +0000

    clear bash history last

--- utils/prepare4img.sh
+++ utils/prepare4img.sh
@@ -15,10 +15,10 @@ rm /root/.gitconfig
 rm -Rf /root/.local
 rm /resize.log
 rm /boot/*
-rm /root/.bash_history
 rm /root/retroarch_fbdev_c1/buildlogs/*
 rm /root/.config/retroarch/playlists/*
-
 cp aafirstboot .first_boot /
 
 echo "File system has been prepared, poweroff and do not reboot"
+rm /root/.bash_history
+
