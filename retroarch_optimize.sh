e2f442db1e3ec8845ee861647ba7cb9363bca211
root <julien@bluemind.org>
2021-06-04 20:51:03 +0000

    changed way fast copper is enabled

--- retroarch_optimize.sh
+++ retroarch_optimize.sh
@@ -7,8 +7,8 @@ function retroarch_optimize() {
     echo " amiberry fast_copper and frameskip for all games..."
     echo "============================================"
     # amiberry : set FAST_COPPER=TRUE in all <hardware> to make a500 games playables on C1
-    sed -i "s/<hardware>/<hardware>FAST_COPPER=TRUE/" /opt/retropie/emulators/amiberry/whdboot/game-data/whdload_db.xml
-    
+    sed -i "s/FAST_COPPER=FALSE/FAST_COPPER=TRUE/" /opt/retropie/emulators/amiberry/whdboot/game-data/whdload_db.xml
+
     cp $ODROIDC1_BUILD_PATH/configs/hostprefs.conf /opt/retropie/emulators/amiberry/whdboot/
 
 	echo ""
@@ -19,10 +19,10 @@ function retroarch_optimize() {
     cp $ODROIDC1_BUILD_PATH/configs/retroarch* /root/.config/retroarch/
     rm /opt/retropie/emulators/retroarch/retroarch.cfg
     ln -s /root/.config/retroarch/retroarch.cfg /opt/retropie/emulators/retroarch/retroarch.cfg
-    
+
     # copy fbneo dat files
     cp $ODROIDC1_BUILD_PATH/configs/*.dat /root/.config/retroarch/
-    
+
     # copy amiberry config file
     cp $ODROIDC1_BUILD_PATH/configs/amiberry.conf /opt/retropie/emulators/amiberry/conf/
 }
